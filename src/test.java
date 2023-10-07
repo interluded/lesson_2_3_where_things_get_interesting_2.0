ba4271160581d9ed4b90074151ca91a51ec5a849
marclmao <marcuslmao1@gmail.com>
2023-10-07 17:59:35 -0400

    part 4

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
         int x = 15;
 
-        if (x < 10)
+        if (x > 10)
         {
             System.out.println("A");
             System.out.println("B");
