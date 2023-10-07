575dc2d4f70d5f366a39509c62acf58b6d2b8270
marclmao <marcuslmao1@gmail.com>
2023-10-07 18:31:05 -0400

    part 6 - 7

--- src/Quiz.java
+++ src/Quiz.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 public class Quiz {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int score = 0;
         System.out.println("What is my name");
         System.out.print(" 1) Marcus \n 2) John \n 3) Not marcus \n 4) Mr. Moorlag \n Enter Your Answer: ");
         int answer_1 = scan.nextInt();
         if (answer_1 == 1){
             System.out.println("You got it right \n");
+            score++;
         } else {
             System.out.println("You got it wrong, the answer was marcus \n ");
         }
@@ -16,6 +18,7 @@ public class Quiz {
         int answer_2 = scan.nextInt();
         if (answer_2 == 1) {
             System.out.println("You got it right");
+            score++;
         } else {
             System.out.println("You got it wrong, the answer was no.");
         }
@@ -24,10 +27,13 @@ public class Quiz {
         int answer_3 = scan.nextInt();
         if (answer_3 == 1) {
             System.out.println("Yeah im completely out");
+            score++;
         }
         else {
             System.out.println("Incorrect, im completely out of ideas");
         }
+        System.out.println("Your score was " + score);
+
     }
 
 
